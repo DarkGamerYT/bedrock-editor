7c4556eee5519b8c3fb3eb1a31f9839236db8244
King <47607185+DarkGamerYT@users.noreply.github.com>
2025-02-17 13:04:30 +0100

    gitignore changes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,24 @@ import * as editor from "@minecraft/server-editor";
 import * as server from "@minecraft/server";
 
 console.log("Hello, world!");
+editor.registerEditorExtension("king:test", (uiSession) => {
+    uiSession.extensionContext;
+    const action = uiSession.actionManager.createAction({
+        actionType: editor.ActionTypes.NoArgsAction,
+        onExecute: () => {
+            
+        },
+    });
+
+    uiSession.actionBar.registerItem("king:actionbar", action, {
+        icon: "pack://textures/editor/blocks_counter.png",
+        label: "Action"
+    });
+    return [];
+}, (uiSession) => {
+
+    return [];
+}, {
+    description: "Hello, world!",
+    notes: "I don't really know."
+});
